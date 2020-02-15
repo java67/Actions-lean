fe964695bcb087e6c3db4c28faaeeb6ad3201fff
lunaticurey <125438787@qq.com>
2020-02-15 22:08:46 +0800

    patch fix

--- diy.sh
+++ diy.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sleep 3
 rm -rf package/lean/luci-app-ssr-plus package/lean/default-settings
-
-for i in $( ls mypatch ); do
-echo Applying mypatch $i
-patch -p1 < mypatch/$i
-done
+sleep 3
+        for i in $( ls mypatch ); do
+            echo Applying mypatch $i
+            patch -p1 < mypatch/$i
+        done
   
